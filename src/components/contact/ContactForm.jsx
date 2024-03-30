5ee678cbdadb1254abd8ed7973baa0283d839e3c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-03-30 10:47:07 +0100

    Suppression du texte de consentement dans le formulaire de contact

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -8,7 +8,6 @@ export const ContactForm = () => {
     const [ message, setMessage ] = useState("");
     const [ isChecked, setIsChecked ] = useState(false);
 
-    const consentText = "En remplissant ce formulaire, vous consentez à fournir ces informations à notre établissement. Vous bénéficiez d'un droit d'accès, de modification, de rectification et de suppression de vos données personnelles. Vous bénéficiez du droit de vous inscrire sur la liste d'opposition au démarchage téléphonique. Pour les exercer, merci de nous contacter."
     const onSubmit = async (e) => {
         e.preventDefault();
         const formData = {
@@ -16,9 +15,7 @@ export const ContactForm = () => {
             email,
             message,
         };
-        if (isChecked) {
         sendContact(formData)
-        }
     }
     return (
         <div id="contact" className="contact">
@@ -32,10 +29,6 @@ export const ContactForm = () => {
                 <input type="email" id="email" className="contact__form-input" placeholder="Email*" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor="message" className="contact__form-label"></label>
                 <textarea name="message" id="message" cols="30" rows="5" className="contact__form-input" placeholder="Message*" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                <div className="contact__form-wrapper">
-                <input type="checkbox" name="consent" id="consent" onChange={()=> setIsChecked(!isChecked)} />
-                <label htmlFor="consent" className="contact__form-label">{consentText}</label>
-                </div>
                 <button type="submit" className="contact__form-btn" onClick={onSubmit}>Envoyer</button>
             </form>
         </div>
